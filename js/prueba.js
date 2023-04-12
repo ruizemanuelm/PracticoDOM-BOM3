54b01819d3cff8fceb1df5f3f51b88238c88e5bf
emanuel ruiz <ruizemanuelok@gmail.com>
2023-04-12 20:57:29 -0300

    etquetas meta y readme

---
+++ js/prueba.js
@@ -0,0 +1,57 @@
+(function(){
+	var lista = document.getElementById("lista"),
+		tareaInput = document.getElementById("tareaInput"),
+		btnNuevaTarea = document.getElementById("btn-agregar");
+
+	var agregarTarea = function(){
+		var tarea = tareaInput.value,
+			nuevaTarea = document.createElement("li"),
+			enlace = document.createElement("a"),
+			contenido = document.createTextNode(tarea);
+
+		if (tarea === "") {
+			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
+			tareaInput.className = "error";
+			return false;
+		}
+
+
+		enlace.appendChild(contenido);
+
+		enlace.setAttribute("href", "#");
+
+		nuevaTarea.appendChild(enlace);
+
+		lista.appendChild(nuevaTarea);
+
+		tareaInput.value = "";
+
+		for (var i = 0; i <= lista.children.length -1; i++) {
+			lista.children[i].addEventListener("click", function(){
+				this.parentNode.removeChild(this);
+			});
+		}
+
+	};
+	var comprobarInput = function(){
+		tareaInput.className = "";
+		teareaInput.setAttribute("placeholder", "Agrega tu tarea");
+	};
+
+	var eleminarTarea = function(){
+		this.parentNode.removeChild(this);
+	};
+
+	// Eventos
+
+	// Agregar Tarea
+	btnNuevaTarea.addEventListener("click", agregarTarea);
+
+	// Comprobar Input
+	tareaInput.addEventListener("click", comprobarInput);
+
+	// Borrando Elementos de la lista
+	for (var i = 0; i <= lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click", eleminarTarea);
+	}
+}());
